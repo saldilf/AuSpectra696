4942483f716ed0f926fcdf39e98c7cf7c170b1cd
Salwan Butrus <salwan@umich.edu>
2018-11-21 13:39:47 -0500

    reading file in function using args.workbook

--- au_spectra696/auspectra696.py
+++ au_spectra696/auspectra696.py
@@ -26,7 +26,7 @@ SUCCESS = 0
 INVALID_DATA = 1
 IO_ERROR = 2
 
-DEFAULT_DATA_FILE_NAME = 'data/LongerTest.xlsx'
+DEFAULT_DATA_FILE_NAME = 'LongerTest.xlsx'
 
 
 def warning(*objs):
@@ -54,7 +54,7 @@ def parse_cmdline(argv):
     args = None
     try:
         args = parser.parse_args(argv)
-        args.wb_data = xlrd.open_workbook(input("Enter a file name (LongerTest.xlsx for testing purposes):  "))
+        #args.wb_data = xlrd.open_workbook(input("Enter a file name (LongerTest.xlsx for testing purposes):  "))
     except IOError as e:
         warning("Problems reading file:", e)
         parser.print_help()
@@ -63,7 +63,8 @@ def parse_cmdline(argv):
     return args, SUCCESS
 
 
-def data_analysis(wb_data):
+def data_analysis(data_file):
+    wb_data = xlrd.open_workbook(data_file)
     sheet1 = wb_data.sheet_by_index(0)
     r = sheet1.nrows
     c = sheet1.ncols
@@ -80,8 +81,49 @@ def data_analysis(wb_data):
                            'Amax': [],
                            'Size (nm)': []
                            })
+    for x in range(3,c):
 
-    print(r,c)
+
+        #299 is the number of data points minus 2
+        lambdas = sheet1.col_values(colx = 0,start_rowx = (xLimU - 299 ) - xLimL,  end_rowx = r) #x-vals (wavelength)
+        abso = sheet1.col_values(colx = x,start_rowx = (xLimU - 299)-xLimL,end_rowx = r) #y-vals (absorbance)
+
+        #400 is the lambda we start plotting at
+        sID = sheet1.cell(0,x).value #name of each column
+        lMax = abso.index(max(abso)) + 400 #find max lambda for a column
+        Amax = max(abso) #get max Abs to norm against it
+
+        absoNorm = [x/Amax for x in abso] #normalizes to Amax
+
+        size = -0.02111514*(lMax**2.0) + 24.6*(lMax) - 7065.
+        #J. Phys. Chem. C 2007, 111, 14664-14669
+
+        #if lambdaMax outisde of this range then 1) particles aggregated and 2)outside of correlation
+        if 518 < lMax < 570:
+            data['Size (nm)'].append(int(size))
+
+        else:
+            data['Size (nm)'].append('>100')
+
+        #added extracted/calculated items into dict 'data'
+        data['Sample ID'].append(sID)
+        data['lambdaMax (nm)'].append(lMax)
+        data['Amax'].append( Amax )
+
+        #plot each column (cycle in loop)
+        plt.plot(lambdas, absoNorm ,linewidth=2,label= sheet1.cell(0,x).value)
+        axes = plt.gca()
+        box = axes.get_position()
+        axes.set_position([box.x0, box.y0, box.width * 0.983, box.height])
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('Absorbance (Normalized to Amax)')
+
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
+    axes.set_xlim([400 , 700])
+    axes.set_ylim([0 , 1.5])
+    plt.show()
+
+
 
 def plot_data(data_stats):
     pass
@@ -92,7 +134,7 @@ def main(argv=None):
     if ret != SUCCESS:
         return ret
 
-    data_stats = data_analysis(args.wb_data)
+    data_stats = data_analysis(args.workbook)
 
     #
     # # get the name of the input file without the directory it is in, if one was specified
