85b28203301a039153ad94f984ac8b3961a00881
Salwan Butrus <salwan@umich.edu>
2018-11-21 19:52:40 -0500

    norm is an option and 2 unit tests passes

--- au_spectra696/auspectra696.py
+++ au_spectra696/auspectra696.py
@@ -25,9 +25,9 @@ SUCCESS = 0
 INVALID_DATA = 1
 IO_ERROR = 2
 
-DEFAULT_DATA_FILE_NAME = 'LongerTest.xlsx'
-#DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
-#DEFAULT_DATA_FILE_NAME = os.path.join(DATA_DIR, "LongerTest.xlsx")
+#DEFAULT_DATA_FILE_NAME = 'LongerTest.xlsx'
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+DEFAULT_DATA_FILE_NAME = os.path.join(DATA_DIR, "LongerTest.xlsx")
 
 
 def warning(*objs):
@@ -64,6 +64,76 @@ def parse_cmdline(argv):
     return args, SUCCESS
 
 
+def norm(rawAbs, Amax, x):
+    absoNorm = [x / Amax for x in rawAbs]  # normalizes to Amax
+    maxAbsAfterNorm = max(absoNorm)
+    return absoNorm
+
+
+def data_analysisNorm(data_file):
+    wb_data = xlrd.open_workbook(data_file)
+    sheet1 = wb_data.sheet_by_index(0)
+    r = sheet1.nrows
+    c = sheet1.ncols
+    xLimLflt = sheet1.cell(1, 0).value  # must be less than 400
+    xLimL = int(xLimLflt)
+
+    # Upper limit
+    xLimUflt = sheet1.cell(r - 1, 0).value
+    xLimU = int(xLimUflt)
+
+    # dict for extracted or calculated values from data
+    data = cl.OrderedDict({'Sample ID': [],
+                           'lambdaMax (nm)': [],
+                           'Amax': [],
+                           'Size (nm)': []
+                           })
+    for x in range(3, c):
+
+        # 299 is the number of data points minus 2
+        lambdas = sheet1.col_values(colx=0, start_rowx=(xLimU - 299) - xLimL, end_rowx=r)  # x-vals (wavelength)
+        abso = sheet1.col_values(colx=x, start_rowx=(xLimU - 299) - xLimL, end_rowx=r)  # y-vals (absorbance)
+
+        # 400 is the lambda we start plotting at
+        sID = sheet1.cell(0, x).value  # name of each column
+        lMax = abso.index(max(abso)) + 400  # find max lambda for a column
+        Amax = max(abso)  # get max Abs to norm against it
+
+        absoNorm = norm(abso, Amax, x)
+       # absoNorm = [x / Amax for x in abso]  # normalizes to Amax
+
+        size = -0.02111514 * (lMax ** 2.0) + 24.6 * (lMax) - 7065.
+        # J. Phys. Chem. C 2007, 111, 14664-14669
+
+        # if lambdaMax outisde of this range then 1) particles aggregated and 2)outside of correlation
+        if 518 < lMax < 570:
+            data['Size (nm)'].append(int(size))
+
+        else:
+            data['Size (nm)'].append('>100')
+
+        # added extracted/calculated items into dict 'data'
+        data['Sample ID'].append(sID)
+        data['lambdaMax (nm)'].append(lMax)
+        data['Amax'].append(Amax)
+
+        # plot each column (cycle in loop)
+        plt.plot(lambdas, absoNorm, linewidth=2, label=sheet1.cell(0, x).value)
+        axes = plt.gca()
+        box = axes.get_position()
+        axes.set_position([box.x0, box.y0, box.width * 0.983, box.height])
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('Absorbance (Normalized to Amax)')
+
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
+    axes.set_xlim([400, 700])
+    axes.set_ylim([0, 1.5])
+    #plt.show()
+    plt.savefig('data/AuPlotNorm.png', format='png', dpi=1000)
+    #plt.savefig('data/AuPlot_Correct.png', format='png', dpi=1000)
+
+    return data['Amax']
+
 def data_analysis(data_file):
     wb_data = xlrd.open_workbook(data_file)
     sheet1 = wb_data.sheet_by_index(0)
@@ -93,7 +163,7 @@ def data_analysis(data_file):
         lMax = abso.index(max(abso)) + 400  # find max lambda for a column
         Amax = max(abso)  # get max Abs to norm against it
 
-        absoNorm = [x / Amax for x in abso]  # normalizes to Amax
+
 
         size = -0.02111514 * (lMax ** 2.0) + 24.6 * (lMax) - 7065.
         # J. Phys. Chem. C 2007, 111, 14664-14669
@@ -111,22 +181,21 @@ def data_analysis(data_file):
         data['Amax'].append(Amax)
 
         # plot each column (cycle in loop)
-        plt.plot(lambdas, absoNorm, linewidth=2, label=sheet1.cell(0, x).value)
+        plt.plot(lambdas, abso, linewidth=2, label=sheet1.cell(0, x).value)
         axes = plt.gca()
         box = axes.get_position()
         axes.set_position([box.x0, box.y0, box.width * 0.983, box.height])
         plt.xlabel('Wavelength (nm)')
-        plt.ylabel('Absorbance (Normalized to Amax)')
+        plt.ylabel('Absorbance')
 
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
     axes.set_xlim([400, 700])
-    axes.set_ylim([0, 1.5])
-    plt.show()
+    axes.set_ylim([0 , Amax + 0.05])
+    #plt.show()
+    plt.savefig('data/AuPlot.png', format='png', dpi=1000)
+    #plt.savefig('data/AuPlot_Correct.png', format='png', dpi=1000)
 
     return data['Amax']
-
-def plot_data(data_stats):
-    pass
 
 
 def main(argv=None):
@@ -140,9 +209,11 @@ def main(argv=None):
     try:
         # set the norm and others in this 'try' so you can pass them into data_analysis()
         if args.normalize is True:
-            #RUN A FUNC THAT NORMS THE DATA THENPLOT
             print("norm is true")
-        data_stats = data_analysis(args.workbook)
+            data_analysisNorm(args.workbook)
+        else:
+            print("norm not true")
+            data_analysis(args.workbook)
         #IF JUST TABLE
         #IF JUST PLOT
     except IOError as e:
@@ -154,5 +225,5 @@ def main(argv=None):
 
 
 if __name__ == "__main__":
-    status = main()
+    status = main(sys.argv[1:])
     sys.exit(status)
--- au_spectra696/test_auspectra696.py
+++ au_spectra696/test_auspectra696.py
@@ -2,8 +2,11 @@
 import unittest
 import os
 import auspectra696
+from auspectra696 import main
+import xlrd
 #from au_spectra696.auspectra696 import main
 import logging
+
 
 
 __author__ = 'salwan'
@@ -22,7 +25,26 @@ DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 EXCEL_INPUT = os.path.join(DATA_DIR, "LongerTest.xlsx")
 
 
+x = 5
+wb_data = xlrd.open_workbook(EXCEL_INPUT)
+sheet1 = wb_data.sheet_by_index(0)
+r = sheet1.nrows
+c = sheet1.ncols
+xLimLflt = sheet1.cell(1, 0).value  # must be less than 400
+xLimL = int(xLimLflt)
+
+# Upper limit
+xLimUflt = sheet1.cell(r - 1, 0).value
+xLimU = int(xLimUflt)
+lambdas = sheet1.col_values(colx=0, start_rowx=(xLimU - 299) - xLimL, end_rowx=r)  # x-vals (wavelength)
+abso = sheet1.col_values(colx=x, start_rowx=(xLimU - 299) - xLimL, end_rowx=r)  # y-vals (absorbance)
+
 class Testauspectra696(unittest.TestCase):
+
+    def test_norm(self):
+        result = auspectra696.norm(abso, max(abso), x)
+        resultF = max(result)
+        self.assertEqual(resultF, 1)
 
     def test_data_analysis(self):
         result = auspectra696.data_analysis(EXCEL_INPUT)
@@ -30,4 +52,14 @@ class Testauspectra696(unittest.TestCase):
                                   0.21041, 0.22364, 0.19213, 0.2101, 0.20578,
                                   0.18737, 0.20889, 0.19202])
 
-   # DEF TEST NORM== ARE ALL LAMBDAMAXES ALL EQUAL TO 1
+    #
+    # def test_data_analysisNorm(self):
+    #     result = auspectra696.data_analysisNorm(EXCEL_INPUT)
+    #     self.assertEqual(result, [0.01287, 0.20669, 0.21599, 0.21233, 0.20997,
+    #                               0.21041, 0.22364, 0.19213, 0.2101, 0.20578,
+    #                               0.18737, 0.20889, 0.19202])
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+# DEF TEST NORM== ARE ALL LAMBDAMAXES ALL EQUAL TO 1
